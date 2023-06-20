94c5f3bec39e4f98d5e71fdbe70e74c8e27e70dc
ravsonga <ravisongare2@gmail.com>
2023-06-20 17:13:47 +0530

    query param api added

--- src/main/java/songare/ravi/myhelloapp/controller/HelloController.java
+++ src/main/java/songare/ravi/myhelloapp/controller/HelloController.java
@@ -1,6 +1,7 @@
 package songare.ravi.myhelloapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +11,13 @@ public class HelloController {
     String hello(){
         System.out.println("Inside hello method");
         return "Hello world";
-        
+
+    }
+
+    @GetMapping("/hello/name")
+    String helloWithQp(@RequestParam String name){
+        System.out.println("Inside hello method:"+name);
+        return "Hello "+name+" !";
+
     }
 }
