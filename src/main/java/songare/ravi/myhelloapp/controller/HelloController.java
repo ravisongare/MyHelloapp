47c7b7b69fe1f6399314d3a852177bbe5e3fb602
ravsonga <ravisongare2@gmail.com>
2023-06-20 17:43:06 +0530

    Hello msg changed

--- src/main/java/songare/ravi/myhelloapp/controller/HelloController.java
+++ src/main/java/songare/ravi/myhelloapp/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     @GetMapping("/hello")
     String hello(){
         System.out.println("Inside hello method");
-        return "Hello world";
+        return "Hello Ravi";
 
     }
 
